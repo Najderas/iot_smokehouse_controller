fad067f9fba21e2c8ea24ed0481b99f3889f7551
Grzegorz Najderek <najderek@student.agh.edu.pl>
2015-06-12 12:47:15 +0200

    drobne poprawki, odkomentowane linijki na copernicusa, wywalone niepotrzebne komentarze

--- project.py
+++ project.py
@@ -5,51 +5,27 @@ from pseudo_pid_controller import PID
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 
+
+#### INITIALIZE #####
+# connect to google spreadsheet
 json_key = json.load(open('SmokehouseController-5be721d918b4.json'))
 scope = ['https://spreadsheets.google.com/feeds']
 
 credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
 
-
-# gc = gspread.login('fourth.copernicus@gmail.com', '%ku5Xw9h{5@Dyzt5')
 gc = gspread.authorize(credentials)
 
-
-#feed = gc.get_spreadsheets_feed()
-# for elem in feed.findall(_ns('entry')):
-#     print elem.find(_ns('title')).text
-#
-# print gc.get_spreadsheets_feed()
 worksheet = gc.open_by_key("1pzZBRAO7qukY5rArVy-YA1tSJ8zQ9Ihckl7degFTNdM").sheet1
 
-
-#### INITIALIZE #####
-# connect to google spreadsheet
-
 # configure copernicus board
-# serial = s.Serial('/dev/ttyS0', 38400, timeout=1)
-# serial.write(chr(128+4))    #automatic update: knob position (as temperature value)
+serial = s.Serial('/dev/ttyS0', 38400, timeout=1)
+serial.write(chr(128+4))    #automatic update: knob position (as temperature value)
 
-# INITIALIZE PID HERE
+# initialize PID 
 p = PID(3.0, 0.4, 1.2)
-
-# Reading cells from the first sheet:
-# query_prefferedTemperatureValueColumn = gdata.spreadsheet.service.CellQuery()
-# query_prefferedTemperatureValueColumn.min_row = '3'
-# query_prefferedTemperatureValueColumn.max_row = '14'
-# query_prefferedTemperatureValueColumn.min_col = '1'
-# query_prefferedTemperatureValueColumn.max_col = '1'
-#
-# query_prefferedTempperatureTimeLeftColumn = gdata.spreadsheet.service.CellQuery()
-# query_prefferedTempperatureTimeLeftColumn.min_row = '3'
-# query_prefferedTempperatureTimeLeftColumn.max_row = '14'
-# query_prefferedTempperatureTimeLeftColumn.min_col = '3'
-# query_prefferedTempperatureTimeLeftColumn.max_col = '3'
 
 min_row = 2
 
-
-# print spr_client.GetCellsFeed(query=query_prefferedTemperatureValueColumn)
 
 #### FUNCTIONS ####
 
@@ -84,39 +60,34 @@ except:
 
 while True:
     # 1: read temperature
-    # cc = serial.read(1)
-    # temperature = temperature(cc);
-    # if temperature > 0:     # check, if readed byte is a new temperature value; if true, react
-    #     currentTemperature = temperature
+    cc = serial.read(1)
+    temperature = temperature(cc);
+    if temperature > 0:     # check, if readed byte is a new temperature value; if true, react
+        currentTemperature = temperature
 
     # 2: update current time, current temperature (if anything changed)
     if currentTime == 0:
-        # spr_client.UpdateCell(2, 9, currentTime, key)  # (row, col, val, key)
         worksheet.update_cell(2, 9, currentTimeInMinutes)  # (row, col, val, key)
     if currentTime % 10 == 0:
-        # spr_client.UpdateCell(2, 10, currentTemperature, key)  # (row, col, val, key)
         worksheet.update_cell(2, 10, currentTemperature)  # (row, col, val, key)
 
     # 3: read columns: preferred temperatue, time left, find current pref. temper.
     if currentTime == 0:
         try:
-            # prefferedTemperatureValueColumn = spr_client.GetCellsFeed(key, query=query_prefferedTemperatureValueColumn)
             prefferedTemperatureValueColumn = worksheet.col_values(1)[min_row:]
             # print prefferedTemperatureValueColumn
-            # prefferedTemperatureTimeLeftColumn = spr_client.GetCellsFeed(key,
-            #                                                              query=query_prefferedTempperatureTimeLeftColumn)
+            
             prefferedTemperatureTimeLeftColumn = worksheet.col_values(3)[min_row:]
             #print prefferedTemperatureTimeLeftColumn
-            # search current preferred temperature (first record in TimeLeft column, that is not zero) read value and write it to preferredTemperature
-
+            
         except:
             print "No connection to spreadsheet."
             # work on lastPrefferedTempperatureValueColumn and lastPrefferedTempperatureTimeLeftColumn
             # do the same as in case of connection to internet ()it should be separate function probably)
 
+        # search current preferred temperature (first record in TimeLeft column, that is not zero) read value and write it to preferredTemperature
         index = 0
         for entry in prefferedTemperatureTimeLeftColumn: #.entry:
-            #print entry
             if int(entry) != 0:
                 break
             index += 1
@@ -127,8 +98,8 @@ while True:
 
     # 4: move valve (servo on Copernicus) to the prescaled value (from airFlowLevel variable)
     airFlowLevel = PIDgetCalculatedValue(int(currentTemperature))
-#    print airFlowLevel
-    # serial.write(airFlowLevel * 31 / 100)
+    # print airFlowLevel
+    serial.write(airFlowLevel * 31 / 100)
 
     # 5: sleep
     time.sleep(1)
